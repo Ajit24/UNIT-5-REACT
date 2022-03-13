9faa1db30b84eb4967fe62d7e00ee68e1fa0a905
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-03-13 10:58:55 +0530

    form

---
+++ day7-forms/react-form/src/App.jsx
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+import Form from './Component/Form'
+
+const App = ()=> {
+  return (
+    <>
+    <Form />
+    </>
+  )
+}
+
+export default App;
